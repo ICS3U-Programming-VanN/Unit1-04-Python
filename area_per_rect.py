ced791f5d614bd4bedc26ba27dc115a23e69a9e1
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-19 00:38:37 +0000

    Finished program code that calculates the area and perimeter of a rectangle.

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Van Nguyen
+# Created on: September 16, 2022
+# This program calculates and displays the area and perimeter of a rectangle with length 13cm and width 2533cm.
+
+
+def main():
+    print("For a rectangle that has dimensions:")
+    print("Length 12cm and width 2533cm:\n")
+    print("Area = {}cm^2".format(13 * 2533))
+    print("Perimeter = {}cm".format(2 * (13 + 2533)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Van Nguyen
+# Created on: September 16, 2022
+# This program prints basic math operations.
+
+
+def main():
+    print("12121 + 1000 = {}".format(12121 + 1000))
+    print("50 - 25 = {}".format(50 - 25))
+    print("30 / 3 = {}".format(30 / 3))
+    print("10 + 15 * 2 = {}".format(10 + 15 * 2))
+    print("2^10 = {}".format(2**10))
+    print("3 / 3 * 10 = {}".format(3 / 3 * 10))
+
+
+if __name__ == "__main__":
+    main()
